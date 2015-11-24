007faa4ee77ad02e3ce8e2f141d20789ebd8cc92
antti.viljakainen <antti.viljakainen@otavanopisto.fi>
2015-11-24 16:12:30 +0200

    - Added tests for 5-dimensional sphere and Rosenbrock functions

--- src/test/java/fi/sondeco/evo/de/TestDiffEvo.java
+++ src/test/java/fi/sondeco/evo/de/TestDiffEvo.java
@@ -10,6 +10,10 @@ import org.junit.Test;
 import fi.sondeco.evo.de.DifferentialEvolution;
 import fi.sondeco.evo.de.DifferentialEvolutionFunction;
 
+/**
+ * 
+ * Stuff to test: https://en.wikipedia.org/wiki/Test_functions_for_optimization
+ */
 public class TestDiffEvo {
 
   @Test
@@ -91,4 +95,79 @@ public class TestDiffEvo {
     }
   }
   
+  @Test
+  public void testSphere() {
+    int parameterCount = 5;
+    double[] min = { -5, -5, -5, -5, -5 };
+    double[] max = { 5, 5, 5, 5, 5 };
+    int populationSize = 10;
+    int maxIterations = 200;
+    double f = 0.8;
+    double cr = 0.9;
+    DifferentialEvolutionFunction e = new DETestF();
+    
+    List<double[]> list = DifferentialEvolution.minimize(
+        parameterCount, min, max, populationSize, maxIterations, f, cr, e);
+
+    System.out.println("Sphere output:");
+    
+    for (double[] x : list) {
+      // Convex functions should converge pretty close to zero
+      for (double d : x)
+        assertEquals(0, d, 0.001);
+      printArray(x, e);
+    }
+  }
+  
+  @Test
+  public void testRosenbrock() {
+    int parameterCount = 5;
+    double[] min = { -5, -5, -5, -5, -5 };
+    double[] max = { 5, 5, 5, 5, 5 };
+    int populationSize = 10;
+    int maxIterations = 2000;
+    double f = 0.8;
+    double cr = 0.9;
+    
+    DifferentialEvolutionFunction def = new DifferentialEvolutionFunction() {
+      public double f(double[] x) {
+        double sum = 0;
+        for (int i = 0; i < x.length - 1; i++) {
+          sum += 100 * Math.pow(x[i + 1] - Math.pow(x[i], 2), 2) + Math.pow(x[i] - 1, 2);
+        }
+        return sum;
+      }
+    };
+    
+    List<double[]> list = DifferentialEvolution.minimize(
+        parameterCount, min, max, populationSize, maxIterations, f, cr, def);
+
+    System.out.println("Rosenbrock output:");
+    
+    for (double[] x : list) {
+//      for (double d : x)
+//        assertEquals(1, d, 0.001);
+      printArray(x, def);
+    }
+  }
+  
+  private void printArray(double[] arr, DifferentialEvolutionFunction def) {
+    System.out.print("[");
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(String.format("%f", arr[i]));
+      
+      if (i != (arr.length - 1))
+        System.out.print(" ");
+    }
+    System.out.print("]");
+    
+    if (def != null) {
+      double fv = def.f(arr);
+      
+      System.out.print(" f(x)=");
+      System.out.print(String.format("%f", fv));
+    }
+    
+    System.out.println();
+  }
 }
